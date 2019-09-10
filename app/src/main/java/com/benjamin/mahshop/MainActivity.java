5acaf30142ff825b452bbce6c4336478f6c5a29a
Benjamin-Mah <mbenjamin0279@gmail.com>
2019-09-10 09:04:30 -0400

    Programmed begin button to start next activity

--- app/src/main/java/com/benjamin/mahshop/MainActivity.java
+++ app/src/main/java/com/benjamin/mahshop/MainActivity.java
@@ -2,7 +2,9 @@ package com.benjamin.mahshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Starts menu
+     * @param view
+     */
+    public void beginApp(View view) {
+        Intent i = new Intent(this, MenuActivity.class);
+        startActivity(i);
     }
 }
