de755b071fd075ee8744631b56145a907fa4ada4
Benjamin-Mah <mbenjamin0279@gmail.com>
2019-09-12 19:59:45 -0400

    Experimented with table layout

--- app/src/main/java/com/benjamin/mahshop/CheckoutActivity.java
+++ app/src/main/java/com/benjamin/mahshop/CheckoutActivity.java
@@ -3,6 +3,10 @@ package com.benjamin.mahshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class CheckoutActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class CheckoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        // Test code
+        TableLayout t = findViewById(R.id.billTable);
+        TableRow tr = new TableRow(this);
+        TextView txt = new TextView(this);
+        txt.setText("1");
+        tr.addView(txt);
+        try {
+            t.addView(tr);
+        }
+        catch (Exception e) {
+            Log.d("Exception err", e.getMessage());
+        }
     }
 }
