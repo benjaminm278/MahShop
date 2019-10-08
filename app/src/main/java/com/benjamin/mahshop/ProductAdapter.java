02a0d2e9519801d7e8a6244d59b99263b6aae1f5
Benjamin-Mah <mbenjamin0279@gmail.com>
2019-10-08 10:10:10 -0400

    Add TextView attributes to ViewHolder class

--- app/src/main/java/com/benjamin/mahshop/ProductAdapter.java
+++ app/src/main/java/com/benjamin/mahshop/ProductAdapter.java
@@ -5,8 +5,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.benjamin.mahshop.model.Item;
@@ -17,11 +20,22 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
     private LinkedList<Item> mListOfItems;
     private LayoutInflater mInflater;
 
+    /**
+     * Creates an adapter by setting the layout inflater and list of items
+     * @param ctx
+     * @param listOfItems
+     */
     public ProductAdapter(Context ctx, LinkedList<Item> listOfItems) {
         mInflater = LayoutInflater.from(ctx); // Inflates adapter
         this.mListOfItems = listOfItems; // Gets list of items
     }
 
+    /**
+     *
+     * @param parent
+     * @param viewType
+     * @return
+     */
     @NonNull
     @Override
     public ProductAdapter.ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -33,23 +47,52 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
         return new ProductViewHolder(myItemView, this);
     }
 
+    /**
+     *
+     * @param holder
+     * @param position
+     */
     @Override
     public void onBindViewHolder(@NonNull ProductAdapter.ProductViewHolder holder, int position) {
         Item x = mListOfItems.get(position);
         Log.d("Pass", "you did it!");
+        holder.nameTxt.setText("Abcdefg");
     }
 
+    /**
+     * Returns the size of the list
+     * @return
+     */
     @Override
     public int getItemCount() {
         return mListOfItems.size();
     }
 
+    /*********************
+     * View holder class *
+     *********************/
     class ProductViewHolder extends RecyclerView.ViewHolder {
+        public final CardView c;
+        public final TextView nameTxt;
+        public final TextView descriptionTxt;
+        public final TextView priceTxt;
+        public final ImageView itemImg;
+        public final TextView quantityTxt;
         final ProductAdapter mAdapter;
+        /**
+         * Creates
+         * @param itemView
+         * @param adapter
+         */
         public ProductViewHolder(@NonNull View itemView, ProductAdapter adapter) {
             super(itemView);
+            c = itemView.findViewById(R.id.ItemCardView);
+            nameTxt = c.findViewById(R.id.name_text);
+            descriptionTxt = c.findViewById(R.id.description_text);
+            priceTxt = c.findViewById(R.id.description_text);
+            itemImg = c.findViewById(R.id.imageView);
+            quantityTxt = c.findViewById(R.id.quantity_text);
             this.mAdapter = adapter;
         }
     }
-
 }
